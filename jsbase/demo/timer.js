f0f12f5c07ce05d3ad02269b23ea743b36bb2f2d
walt2016 <22802768@qq.com>
2018-11-20 16:46:57 +0800

    1

--- jsbase/demo/timer.js
+++ jsbase/demo/timer.js
@@ -2,14 +2,15 @@ var startTime=+new Date();
 var timer;
 var fn=function(){
 	var endTime=+new Date()
-	var offset=endTime-startTime
-	if( offset>10000){
+	var interval=endTime-startTime
+	if( interval>5000){
 		console.log("clearTimeout")
 		timer&&clearTimeout(timer)
 		return
 	}
-	console.log(offset)
+	console.log(interval)
 	timer=setTimeout(fn,1000)
 }
 
 fn()
+
